347b2256f068d633e11782d01ff7e18c8b6f8d9b
heesu-ahn <anhesu4@gmail.com>
2021-07-02 18:01:05 +0900

    create_new_repository

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-n34_*@)3kfns3@pim(%d5*u)^q9!j(#h-dm@2ibcg6+ce$c=4f'
